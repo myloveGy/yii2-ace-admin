8750c5bf27899b2c69774aef7ede48d49e03f1aa
liujinxing <821901008@qq.com>
2018-01-23 16:08:11 +0800

    前端代码优化

--- backend/config/params.php
+++ backend/config/params.php
@@ -7,4 +7,5 @@ return [
     'projectName' => 'Yii2 后台管理系统',               // 项目名称
     'projectTitle' => 'Yii2 后台管理系统',
     'companyName' => '<span class="blue bolder"> Liujinxing </span> Yii2 Admin 项目 &copy; 2016-2018',
+    'iframeNumberSize' => 8,                           // 允许页面打开几个iframe窗口
 ];
