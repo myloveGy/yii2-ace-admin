e40e70bcc16c2147be3b659b26fd3a50ab5a20c7
jinxing.liu <821901008@qq.com>
2018-06-04 22:27:39 +0800

    使用yii2-admin

--- backend/controllers/Controller.php
+++ backend/controllers/Controller.php
@@ -3,14 +3,25 @@
 namespace backend\controllers;
 
 use jinxing\admin\behaviors\AccessControl;
+use jinxing\admin\controllers\Controller as BaseController;
 
 /**
  * Class Controller 后台的基础控制器
  * @author  liujx
  * @package backend\controllers
  */
-class Controller extends \yii\web\Controller
+class Controller extends BaseController
 {
+    /**
+     * @var string 使用yii2-admin 的布局
+     */
+    public $layout = '@jinxing/admin/views/layouts/main';
+
+    /**
+     * 定义使用的行为
+     *
+     * @return array
+     */
     public function behaviors()
     {
         return [
--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -17,31 +17,33 @@ class UserController extends Controller
 
     /**
      * 查询处理
-     * @param  array $params
+     *
      * @return array 返回数组
      */
-    public function where($params)
+    public function where()
     {
         return [
             'username' => 'like',
-            'email' => 'like',
+            'email'    => 'like',
         ];
     }
 
     /**
      * 首页显示
+     *
      * @return string
      */
     public function actionIndex()
     {
         return $this->render('index', [
-            'status' => User::getArrayStatus(),
+            'status'      => User::getArrayStatus(),
             'statusColor' => User::getStatusColor(),
         ]);
     }
 
     /**
      * 处理导出数据显示的问题
+     *
      * @return array
      */
     public function getExportHandleParams()
