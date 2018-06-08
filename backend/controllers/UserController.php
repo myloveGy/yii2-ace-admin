ae9733cfab190e64331536fb845bff90bb7b1f23
liujinxing <jinxing.liu@verystar.cn>
2018-06-08 16:45:16 +0800

    重新初始化项目

---
+++ backend/controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace backend\controllers;
+
+use backend\models\User;
+
+/**
+ * Class UserController 用户信息
+ * @package backend\controllers
+ */
+class UserController extends Controller
+{
+    /**
+     * @var string 定义使用的model
+     */
+    public $modelClass = 'backend\models\User';
+
+    /**
+     * 查询处理
+     *
+     * @return array 返回数组
+     */
+    public function where()
+    {
+        return [
+            'username' => 'like',
+            'email'    => 'like',
+        ];
+    }
+
+    /**
+     * 首页显示
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index', [
+            'status'      => User::getArrayStatus(),
+            'statusColor' => User::getStatusColor(),
+        ]);
+    }
+
+    /**
+     * 处理导出数据显示的问题
+     *
+     * @return array
+     */
+    public function getExportHandleParams()
+    {
+        $array['created_at'] = $array['updated_at'] = function ($value) {
+            return date('Y-m-d H:i:s', $value);
+        };
+
+        return $array;
+    }
+}
+
