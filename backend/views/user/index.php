724492ebbebf9210ebfe57a1bacbe68a6129b040
liujinxing <jinxing.liu@verystar.cn>
2018-11-13 14:44:00 +0800

    导出优化

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -14,90 +14,70 @@ $this->title = '用户信息';
             m = meTables({
                 title: "用户信息",
                 table: {
-                    "aoColumns": [
+                    aoColumns: [
                         {
-                            "title": "id",
-                            "data": "id", "edit": {"type": "hidden"},
-                            "bSortable": false
+                            title: "id",
+                            data: "id", "edit": {"type": "hidden"},
+                            sortable: false
                         },
                         {
-                            "title": "用户名",
-                            "data": "username",
-                            "edit": {"type": "text", "required": true, "rangelength": "[2, 255]"},
-                            "bSortable": false
+                            title: "用户名",
+                            data: "username",
+                            edit: {type: "text", required: true, rangeLength: "[2, 255]"},
+                            sortable: false
                         },
                         {
-                            "title": "密码",
-                            "data": "password",
-                            "bHide": true,
-                            "edit": {"type": "password", "rangelength": "[2, 20]"},
-                            "bSortable": false,
-                            "defaultContent": "",
-                            "bViews": false
+                            title: "密码",
+                            data: "password",
+                            hide: true,
+                            edit: {type: "password", rangeLength: "[2, 20]"},
+                            sortable: false,
+                            defaultContent: "",
+                            view: false
                         },
                         {
-                            "title": "确认密码",
-                            "data": "repassword",
-                            "bHide": true,
-                            "edit": {
-                                "type": "password",
-                                "rangelength": "[2, 20]",
-                                "equalTo": "input[name=password]:first"
+                            title: "确认密码",
+                            data: "repassword",
+                            hide: true,
+                            edit: {
+                                type: "password",
+                                rangeLength: "[2, 20]",
+                                equalTo: "input[name=password]:first"
                             },
-                            "bSortable": false,
-                            "defaultContent": "",
-                            "bViews": false
+                            sortable: false,
+                            defaultContent: "",
+                            view: false
                         },
                         {
-                            "title": "Email",
-                            "data": "email",
-                            "edit": {"type": "text", "required": true, email: true, "rangelength": "[2, 255]"},
-                            "bSortable": false
+                            title: "Email",
+                            data: "email",
+                            edit: {type: "text", required: true, email: true, rangeLength: "[2, 255]"},
+                            sortable: false
                         },
                         {
-                            "title": "状态",
-                            "data": "status",
+                            title: "状态",
+                            data: "status",
                             value: arrStatus,
-                            "edit": {"type": "radio", "required": true, "number": true, "default": 10},
+                            edit: {type: "radio", required: true, number: true, default: 10},
                             createdCell: function (td, data) {
-                                $(td).html(mt.valuesString(arrStatus, arrColors, data));
+                                $(td).html(MeTables.valuesString(arrStatus, arrColors, data));
                             },
-                            "bSortable": false
+                            sortable: false
                         },
                         {
-                            "title": "创建时间",
-                            "data": "created_at",
+                            title: "创建时间",
+                            data: "created_at",
                             defaultOrder: "desc",
-                            "createdCell": meTables.dateTimeString
+                            createdCell: MeTables.dateTimeString
                         },
                         {
-                            "title": "修改时间",
-                            "data": "updated_at",
-                            "createdCell": meTables.dateTimeString
+                            title: "修改时间",
+                            data: "updated_at",
+                            createdCell: MeTables.dateTimeString
                         }
                     ]
                 }
             });
-
-        /**
-         meTables.fn.extend({
-        // 显示的前置和后置操作
-        beforeShow: function(data, child) {
-            return true;
-        },
-        afterShow: function(data, child) {
-            return true;
-        },
-        
-        // 编辑的前置和后置操作
-        beforeSave: function(data, child) {
-            return true;
-        },
-        afterSave: function(data, child) {
-            return true;
-        }
-    });
-         */
 
         $(function () {
             m.init();
