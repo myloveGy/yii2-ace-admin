c5db01352e65a9bd77885357760dd9d8249a794e
刘金星 <821901008@qq.com>
2019-06-29 22:51:18 +0800

    refactor: 登录提示可以使用邮箱登录

--- backend/config/main.php
+++ backend/config/main.php
@@ -42,22 +42,9 @@ return [
                 ],
             ],
         ],
+
         'errorHandler' => [
             'errorAction' => 'admin/default/error',
-        ],
-
-        'assetManager' => [
-            'bundles' => [
-                // 去掉自己的bootstrap 资源
-                'yii\bootstrap\BootstrapAsset' => [
-                    'css' => [],
-                ],
-                // 去掉自己加载的Jquery
-                'yii\web\JqueryAsset'          => [
-                    'sourcePath' => null,
-                    'js'         => [],
-                ],
-            ],
         ],
 
         // authority management
