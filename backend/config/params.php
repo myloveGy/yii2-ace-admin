ef50549eb57d6a6bdbee6a6b637dd32d6a645be5
刘金星 <821901008@qq.com>
2020-05-28 00:32:26 +0800

    feat: 页面小优化

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,5 +1,11 @@
 <?php
 return [
     'adminEmail'         => 'admin@example.com',
-    'project_open_other' => true
+    'project_open_other' => true,
+
+//    // 配置登录公司名称
+//    'loginCompanyName'   => '公司名称',
+//
+//    // 页脚公司名称信息
+//    'companyName'        => '公司名称',
 ];
